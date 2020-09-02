f0ad2d8bd0f3056fe0ae529c296c79db3b8ad474
MercuriusXeno <mercuriusxeno@gmail.com>
2020-09-02 15:43:53 -0500

    Bit of datagen and model tweaks

--- src/main/java/com/xeno/goo/datagen/Items.java
+++ src/main/java/com/xeno/goo/datagen/Items.java
@@ -2,6 +2,7 @@ package com.xeno.goo.datagen;
 
 import com.xeno.goo.GooMod;
 import com.xeno.goo.setup.Registry;
+import net.minecraft.client.renderer.model.BuiltInModel;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.*;
@@ -19,7 +20,6 @@ public class Items extends ItemModelProvider {
         registerComboGauntlet();
         registerCrucible();
         registerMobiusCrucible();
-        registerGooBulb();
     }
 
     private void registerMobiusCrucible()
@@ -46,56 +46,5 @@ public class Items extends ItemModelProvider {
     {
         singleTexture(Registry.GASKET.get().getRegistryName().getPath(), new ResourceLocation("item/handheld"),
                 "layer0", new ResourceLocation(GooMod.MOD_ID, "item/gasket"));
-    }
-
-    private void registerGooBulb()
-    {
-        withExistingParent(Registry.GOO_BULB.get().getRegistryName().getPath(), new ResourceLocation("builtin/entity"))
-                .transforms()
-                // GUI
-                .transform(ModelBuilder.Perspective.GUI)
-                .rotation(30, 225, 0)
-                .translation(0, 0, 0)
-                .scale(0.625f, 0.625f, 0.625f)
-                .end()
-                // GROUND
-                .transform(ModelBuilder.Perspective.GROUND)
-                .rotation(0, 0, 0)
-                .translation(0, 3, 0)
-                .scale(0.25f, 0.25f, 0.25f)
-                .end()
-                // FIXED
-                .transform(ModelBuilder.Perspective.FIXED)
-                .rotation(0, 0, 0)
-                .translation(0, 0, 0)
-                .scale(0.5f, 0.5f, 0.5f)
-                .end()
-                // THIRDPERSON_RIGHTHAND
-                .transform(ModelBuilder.Perspective.THIRDPERSON_RIGHT)
-                .rotation(75, 45, 0)
-                .translation(0, 2.5f, 0)
-                .scale(0.375f, 0.375f, 0.375f)
-                .end()
-                // THIRDPERSON_LEFTHAND
-                .transform(ModelBuilder.Perspective.THIRDPERSON_LEFT)
-                .rotation(75, 225, 0)
-                .translation(0, 2.5f, 0)
-                .scale(0.375f, 0.375f, 0.375f)
-                .end()
-                // FIRSTPERSON_RIGHTHAND
-                .transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT)
-                .rotation(0, 45, 0)
-                .translation(0, 0, 0)
-                .scale(0.4f, 0.4f, 0.4f)
-                .end()
-                // FIRSTPERSON_LEFTHAND
-                .transform(ModelBuilder.Perspective.FIRSTPERSON_LEFT)
-                .rotation(0, 225, 0)
-                .translation(0, 0, 0)
-                .scale(0.4f, 0.4f, 0.4f)
-                .end()
-        ;
-
-
     }
 }
