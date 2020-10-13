4f893705efdb6ed80757d8283599fefbb96802fe
MercuriusXeno <mercuriusxeno@gmail.com>
2020-10-13 13:34:48 -0500

    Patchouli adds and fixing Goo Bulb inventory diffuse lighting

--- src/main/java/com/xeno/goo/client/render/GooRenderHelper.java
+++ src/main/java/com/xeno/goo/client/render/GooRenderHelper.java
@@ -44,7 +44,7 @@ public class GooRenderHelper extends RenderState
                 DefaultVertexFormats.POSITION_COLOR_TEX_LIGHTMAP, GL11.GL_QUADS, 256, true, true,
                 RenderType.State.getBuilder()
                         .texture(RenderType.BLOCK_SHEET_MIPPED)
-                        .diffuseLighting(enableDiffuse)
+                        .diffuseLighting(disableDiffuse)
                         .shadeModel(notSmoothShade)
                         .lightmap(RenderType.LIGHTMAP_DISABLED)
                         .transparency(RenderType.TRANSLUCENT_TRANSPARENCY)
