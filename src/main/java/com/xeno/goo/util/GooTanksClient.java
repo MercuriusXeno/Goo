cc2369579ea6038e57a64c56dd9705256d0a841b
skyboy <skyboy026@gmail.com>
2021-04-22 11:47:30 -0400

    Create GooTanksClient.java

---
+++ src/main/java/com/xeno/goo/util/GooTanksClient.java
@@ -0,0 +1,46 @@
+package com.xeno.goo.util;
+
+import net.minecraft.fluid.Fluid;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraftforge.common.util.Constants.NBT;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.function.IntSupplier;
+
+public class GooTanksClient extends GooTanks {
+
+	public GooTanksClient(IntSupplier capacity) {
+
+		super(capacity);
+	}
+
+	@Override
+	protected void readFromNBTInternal(CompoundNBT nbt) {
+
+		final ListNBT tankList = nbt.getList("Tanks", NBT.TAG_COMPOUND);
+
+		FluidStack[] tanks = new FluidStack[tankList.size()];
+		HashMap<Fluid, FluidStack> contents = new HashMap<>();
+
+		int count = 0, amt = 0;
+
+		for (int i = 0, e = tankList.size(); i < e; ++i) {
+			FluidStack tank = FluidStack.loadFluidStackFromNBT(tankList.getCompound(i));
+			if (!contents.containsKey(tank.getRawFluid()) && filter.test(tank)) {
+				contents.put(tank.getRawFluid(), tanks[count++] = tank);
+				amt += tank.getAmount();
+			}
+		}
+
+		Arrays.sort(tanks, Comparator.comparingInt(FluidStack::getAmount));
+
+		tankCount = count;
+		amount = amt;
+		this.tanks = tanks;
+		this.contents = contents;
+	}
+}
