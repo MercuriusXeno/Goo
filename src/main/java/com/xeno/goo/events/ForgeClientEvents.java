005e367154a3b20ae3ec64926a1fbbb861b40e74
skyboy <skyboy026@gmail.com>
2021-05-19 02:17:51 -0400

    Display morehelpful information when goo is unavailable

--- src/main/java/com/xeno/goo/events/ForgeClientEvents.java
+++ src/main/java/com/xeno/goo/events/ForgeClientEvents.java
@@ -2,14 +2,10 @@ package com.xeno.goo.events;
 
 import com.xeno.goo.GooMod;
 import com.xeno.goo.shrink.api.ShrinkAPI;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.MouseHelper;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
-import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderTooltipEvent;
@@ -17,12 +13,18 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = GooMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeClientEvents
 {
     private static final String EVENT_PHASE_FOR_OVERLAY_INJECTION = "ALL";
+
+    @SubscribeEvent
+    public static void onLoggedIn(ClientPlayerNetworkEvent.LoggedInEvent event) {
+
+        // TODO: this is part of a nicer unavailable gooification display
+        TargetingHandler.isGooReady = false;
+    }
 
     @SubscribeEvent
     public static void onDrawTooltip(ItemTooltipEvent event) {
